7f6e70d163fed467835c77cfd3e0cb39b86d67f4
Brian <brian@Kendalls-MacBook-Pro.local>
2019-03-16 10:26:58 -0700

    form linked

--- server.js
+++ server.js
@@ -12,16 +12,54 @@ app.use(express.static('public'));
 app.get('/client/account/addpayment.html', function (req, res) {
     res.sendFile(__dirname + "/client/account/" + "addpayment.html");
 })
+app.post('/client/account/addpayment.html/post', urlencodedParser, function (req, res) {
+    // Prepare output in JSON format
+    response = {
+        first_name: req.body.first_name,
+        last_name: req.body.last_name
+    };
+    console.log(response);
+    res.end(JSON.stringify(response));
+})
+
 app.get('/client/account/register.html', function (req, res) {
     res.sendFile(__dirname + "/client/account/" + "register.html");
+})
+app.post('/client/account/register.html/post', urlencodedParser, function (req, res) {
+    // Prepare output in JSON format
+    response = {
+        first_name: req.body.first_name,
+        last_name: req.body.last_name
+    };
+    console.log(response);
+    res.end(JSON.stringify(response));
 })
 
 // project
 app.get('/client/project/approvescope.html', function (req, res) {
     res.sendFile(__dirname + "/client/project/" + "approvescope.html");
 })
+app.post('/client/project/approvescope.html/post', urlencodedParser, function (req, res) {
+    // Prepare output in JSON format
+    response = {
+        first_name: req.body.first_name,
+        last_name: req.body.last_name
+    };
+    console.log(response);
+    res.end(JSON.stringify(response));
+})
+
 app.get('/client/project/dashboard.html', function (req, res) {
     res.sendFile(__dirname + "/client/project/" + "dashboard.html");
+})
+app.post('/client/project/dashboard.html/post', urlencodedParser, function (req, res) {
+    // Prepare output in JSON format
+    response = {
+        first_name: req.body.first_name,
+        last_name: req.body.last_name
+    };
+    console.log(response);
+    res.end(JSON.stringify(response));
 })
 
 // FREELANCER PAGES
@@ -29,28 +67,104 @@ app.get('/client/project/dashboard.html', function (req, res) {
 app.get('/freelancer/account/addpayment.html', function (req, res) {
     res.sendFile(__dirname + "/freelancer/account/" + "addpayment.html");
 })
+app.post('/freelancer/account/addpayment.html/post', urlencodedParser, function (req, res) {
+    // Prepare output in JSON format
+    response = {
+        first_name: req.body.first_name,
+        last_name: req.body.last_name
+    };
+    console.log(response);
+    res.end(JSON.stringify(response));
+})
 app.get('/freelancer/account/login.html', function (req, res) {
     res.sendFile(__dirname + "/freelancer/account/" + "login.html");
 })
+app.post('/freelancer/account/login.html/post', urlencodedParser, function (req, res) {
+    // Prepare output in JSON format
+    response = {
+        first_name: req.body.first_name,
+        last_name: req.body.last_name
+    };
+    console.log(response);
+    res.end(JSON.stringify(response));
+})
 app.get('/freelancer/account/register.html', function (req, res) {
     res.sendFile(__dirname + "/freelancer/account/" + "register.html");
+})
+app.post('/freelancer/account/register.html/post', urlencodedParser, function (req, res) {
+    // Prepare output in JSON format
+    response = {
+        first_name: req.body.first_name,
+        last_name: req.body.last_name
+    };
+    console.log(response);
+    res.end(JSON.stringify(response));
 })
 
 // project
 app.get('/freelancer/project/addclient.html', function (req, res) {
     res.sendFile(__dirname + "/freelancer/project/" + "addclient.html");
 })
+app.post('/freelancer/project/addclient.html/post', urlencodedParser, function (req, res) {
+    // Prepare output in JSON format
+    response = {
+        first_name: req.body.first_name,
+        last_name: req.body.last_name
+    };
+    console.log(response);
+    res.end(JSON.stringify(response));
+})
+
 app.get('/freelancer/project/addmilestones.html', function (req, res) {
     res.sendFile(__dirname + "/freelancer/project/" + "addmilestones.html");
 })
+app.post('/freelancer/project/addmilestones.html/post', urlencodedParser, function (req, res) {
+    // Prepare output in JSON format
+    response = {
+        first_name: req.body.first_name,
+        last_name: req.body.last_name
+    };
+    console.log(response);
+    res.end(JSON.stringify(response));
+})
+
 app.get('/freelancer/project/dashboard.html', function (req, res) {
     res.sendFile(__dirname + "/freelancer/project/" + "dashboard.html");
 })
+app.post('/freelancer/project/dashboard.html/post', urlencodedParser, function (req, res) {
+    // Prepare output in JSON format
+    response = {
+        first_name: req.body.first_name,
+        last_name: req.body.last_name
+    };
+    console.log(response);
+    res.end(JSON.stringify(response));
+})
+
 app.get('/freelancer/project/fillmilestones.html', function (req, res) {
     res.sendFile(__dirname + "/freelancer/project/" + "fillmilestones.html");
 })
+app.post('/freelancer/project/fillmilestones.html/post', urlencodedParser, function (req, res) {
+    // Prepare output in JSON format
+    response = {
+        first_name: req.body.first_name,
+        last_name: req.body.last_name
+    };
+    console.log(response);
+    res.end(JSON.stringify(response));
+})
+
 app.get('/freelancer/project/reviewproject.html', function (req, res) {
     res.sendFile(__dirname + "/freelancer/project/" + "reviewproject.html");
+})
+app.post('/freelancer/project/reviewproject.html/post', urlencodedParser, function (req, res) {
+    // Prepare output in JSON format
+    response = {
+        first_name: req.body.first_name,
+        last_name: req.body.last_name
+    };
+    console.log(response);
+    res.end(JSON.stringify(response));
 })
 
 // external
