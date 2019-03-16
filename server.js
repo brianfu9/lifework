94a7bb22db6b87b6b3ad4270ce78d1f649ff16c6
Jordan Chernof <jordan.chernof@gmail.com>
2019-03-16 09:53:53 -0700

    add blank html files

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 app.use(express.static('public'));
 app.get('/index.htm', function (req, res) {
-   res.sendFile( __dirname + "/" + "index.htm" );
+   res.sendFile( __dirname + "/" + "index.html" );
 })
 
 app.post('/process_post', urlencodedParser, function (req, res) {
