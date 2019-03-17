93866fc03f9545fc62e95e05d72bb515d1ff3eb2
Jordan Chernof <jordan.chernof@gmail.com>
2019-03-17 13:38:54 -0700

    kicks u out

--- public/js/validatelogin.js
+++ public/js/validatelogin.js
@@ -1,8 +1,6 @@
 $.get("http://127.0.0.1:8081/user_name", function (data) {
-    if (data) {
-        console.log("user is logged in")
-        $("#header").load("/freelancer/logged_in_nav.html");
-    } else {
-        $("#header").load("/freelancer/logged_out_nav.html");
-    }
+    if (!data) {
+        alert('Please log in to view this page.');
+        window.location.href = "/freelancer/account/login.html";
+    } 
 });
